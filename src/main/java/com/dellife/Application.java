fe1be2c0cedc892b20a38823b5410d2652c69d91
dellife <seheekwon21@gmail.com>
2019-10-24 00:28:54 +0900

    springboot 자동설정 이해

--- src/main/java/com/dellife/Application.java
+++ src/main/java/com/dellife/Application.java
@@ -1,12 +1,22 @@
 package com.dellife;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+//@SpringBootApplication
+@ComponentScan
+@Configuration
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
     }
 }
