bd47c026c31a86c90e6ea5dade70a81aa289e1ed
dellife <seheekwon21@gmail.com>
2020-01-16 08:58:49 +0900

    TEST - SpringBootTest.MOCK

---
+++ src/main/java/com/dellife/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.dellife.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "sehee";
+    }
+}
